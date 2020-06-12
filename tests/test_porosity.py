91f1bf247ef54c01b133c022381b4ba5dbcad4cb
Andy <andymcdonald1985@gmail.com>
2020-06-12 20:52:48 +0100

    deliberate failing test to test Travis CI

--- tests/test_porosity.py
+++ tests/test_porosity.py
@@ -7,7 +7,7 @@ class TestPorosity(unittest.TestCase):
     poro = Porosity()
 
     def test_porosity_density(self):
-        self.assertAlmostEqual(self.poro.porosity_density(2.65, 1, 2.45), 0.1379, delta=0.01)
+        self.assertAlmostEqual(self.poro.porosity_density(2.65, 1, 2.45), 0.379, delta=0.01)
         self.assertAlmostEqual(self.poro.porosity_density(2.71, 1, 2.45), 0.1793, delta=0.01)
         
         self.assertEqual(self.poro.porosity_density(2.65, 1, 2.64, limit_result=True, low_limit=0.1, high_limit=1), 0.1)
